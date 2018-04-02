d80e74bc7ee247eef5e34c4cde04836429ded965
Jing Chun Tan <jtan1993@gmail.com>
2018-04-02 22:56:49 +0100

    Add files via upload

---
+++ SetupLocalPlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	public override void OnStartServer()
+	{
+		Debug.Log("Starting Server"  + isLocalPlayer);
+	}
+
+	public override void OnStartClient()
+	{
+		Debug.Log("Starting Client"  + isLocalPlayer);
+	}
+
+	void Awake()
+	{
+		Debug.Log("Awaking Player " + isLocalPlayer);
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		if(isLocalPlayer)
+		{
+			GetComponent<PlayerController>().enabled = true;
+		}
+		else
+		{
+			GetComponent<PlayerController>().enabled = false;
+		}
+		Debug.Log("Starting Player " + isLocalPlayer);
+	}
+
+
+}
